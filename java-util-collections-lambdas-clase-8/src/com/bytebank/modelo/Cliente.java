6b458b09150836f8b38f912215a343bd395f462d
gabarri <68142877+gabarri@users.noreply.github.com>
2023-10-11 08:03:19 -0600

    Update Cliente.java

--- java-util-collections-lambdas-clase-8/src/com/bytebank/modelo/Cliente.java
+++ java-util-collections-lambdas-clase-8/src/com/bytebank/modelo/Cliente.java
@@ -3,7 +3,7 @@ package com.bytebank.modelo;
 public class Cliente implements Autenticable {
 
 	// Public es el mas accesible
-	// Protected 
+	// Protected jjjjjj
 	
 	private String nombre;
     private String documento;
